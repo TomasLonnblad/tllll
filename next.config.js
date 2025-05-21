4f3383ae46ca6527c9ff1105d0345da940982562
TomasLonnblad <tomaslonnblad@gmail.com>
2025-05-21 15:35:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/https://github.com/TomasLonnblad/tlll",
+  basePath: "/https://github.com/TomasLonnblad/tllll",
 };
 
 module.exports = nextConfig;
